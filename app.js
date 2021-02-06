cb6a7765559a38bd54996efc13b833c796ee13ab
sacosta5 <simon.jr.acosta@gmail.com>
2021-02-05 19:39:53 -0700

    my first inquirer

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name', 
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+//const fs = require('fs');
+//const { generate } = require('rxjs');
+//const generatePage = require('./src/page-template');
+//const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//fs.writeFile('./index.html', pageHTML, err => {
+    //if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
 
-console.log(generatePage('Jane', 'janehub'));
+//console.log(generatePage('Jane', 'janehub'));
 
 //const printProfileData = profileDataArr => {
     // This ...
